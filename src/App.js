48028c9fc3b2250e77acad570f126f2f432923d9
Gwang Hyun Ji <gwgw4578@gmail.com>
2021-01-12 23:24:19 +0900

    21 01 12 Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+
+class TOC extends Component{
+  render() {
+    return (
+      <nav>
+      <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+      </ul>
+      </nav>
+    );
+  }
+}
+class Content extends Component{
+  render() {
+    return (
+      <div>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </div>
+    );
+  }
+}
+
+
+class Subject extends Component{
+  render() {
+    return (
+      <header>
+            <h1>WEB</h1>
+            world wide web!
+        </header>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      Hello React!
+      <Subject></Subject>
+      <TOC></TOC>
+      <Content></Content>
     </div>
   );
 }
