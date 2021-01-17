9a54e2bdd49affe30e040b928af60455c814e61f
Gwang Hyun Ji <gwgw4578@gmail.com>
2021-01-18 03:07:53 +0900

    21 01 18 react state

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import Content from "./components/Content";
 import Subject from "./components/Subject";
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      Subject:{title:'WEB',sub:'World Wid Web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:1, title:'CSS', desc:'CSS is for design'},
+        {id:1, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
+  render(){
   return (
     <div className="App">
-      <Subject title="WEB" sub="World wide web!"></Subject>
-      <Subject title="React" sub="For Ui"></Subject>
-      <TOC></TOC>
+      <Subject
+        title={this.state.Subject.title}
+        sub={this.state.Subject.sub}>
+      </Subject>
+      <TOC data={this.state.contents}></TOC>
       <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
     </div>
   );
+}
 }
 
 export default App;
